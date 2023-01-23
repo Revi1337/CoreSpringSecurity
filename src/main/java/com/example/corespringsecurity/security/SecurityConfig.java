e9170333b2f467c33c130a553b1caebc37fad50d
Revi1337 <david122123@gmail.com>
2023-01-24 06:18:42 +0900

    Customize HttpSecurity & WebSecurity
    * Add Default Users
    * Exclude Static Resources

---
+++ src/main/java/com/example/corespringsecurity/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.corespringsecurity.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .mvcMatchers("/").permitAll()
+                        .mvcMatchers("/mypage").hasRole("USER")
+                        .mvcMatchers("/messages").hasRole("MANAGER")
+                        .mvcMatchers("/config").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin()
+                .and()
+                .build();
+    }
+
+    /**
+     * <p> Exclude In Spring Security Control. (Contains Static Resources)
+     * <p> Ex) css, images, js , etc..
+     * @return  {@link WebSecurity}.
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web
+                .ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        String password = passwordEncoder().encode("1111");
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user").password(password).roles("USER").build(),
+                User.withUsername("manager").password(password).roles("MANAGER").build(),
+                User.withUsername("admin").password(password).roles("ADMIN").build()
+        );
+    }
+
+}
