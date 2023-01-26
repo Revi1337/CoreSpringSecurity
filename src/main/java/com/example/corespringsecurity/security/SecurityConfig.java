4d4c64e9626fb60ca4ce5fa7ddbf7d0d37acb936
Revi1337 <david122123@gmail.com>
2023-01-26 22:12:08 +0900

    Form Authentication - CustomAuthenticationProvider
    Form Authentication - CustomAuthenticationProvider

--- src/main/java/com/example/corespringsecurity/security/SecurityConfig.java
+++ src/main/java/com/example/corespringsecurity/security/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.example.corespringsecurity.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -15,28 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity @RequiredArgsConstructor
 public class SecurityConfig {
-    
-    private final CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(customUserDetailsService);
-        return authenticationProvider;
-    }
-
-    /**
-     * <p> The following paths are unconditionally inspected by SecurityFilter
-     * <p> Refer : {@link org.springframework.security.web.access.intercept.FilterSecurityInterceptor} </p>
-     * @param httpSecurity
-     * @return
-     * @throws Exception
-     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -51,13 +36,6 @@ public class SecurityConfig {
                 .build();
     }
 
-    /**
-     * <p> The paths below are filtered before the SecurityFilter. So paths below do not go through the SecurityFilter
-     * <p> Refer : {@link org.springframework.security.web.access.intercept.FilterSecurityInterceptor} </p>
-     * <p> Exclude In Spring Security Control. (Contains Static Resources)
-     * <p> Ex) css, images, js , etc..
-     * @return  {@link WebSecurity}.
-     */
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web
@@ -65,14 +43,14 @@ public class SecurityConfig {
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
-
-    //    @Bean
-//    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-//        String password = passwordEncoder().encode("1111");
-//        return new InMemoryUserDetailsManager(
-//                User.withUsername("user").password(password).roles("USER").build(),
-//                User.withUsername("manager").password(password).roles("USER", "MANAGER").build(),
-//                User.withUsername("admin").password(password).roles("USER", "MANAGER", "ADMIN").build()
-//        );
+//    private final CustomUserDetailsService customUserDetailsService;
+//    /**
+//     * <p>Used for Register CustomUserDetailsService</p>
+//     * @return {@link AuthenticationManager}
+//     */
+//    @Bean
+//    public AuthenticationManager authenticationManager() {
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
 //    }
 }
